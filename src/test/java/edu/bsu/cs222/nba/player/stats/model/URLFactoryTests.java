5264f7f90110c6826ff98d094b8ac02424e4926f
Curtis Clemmons <cpclemmons@bsu.edu>
2020-02-25 02:09:28 -0500

    remove setUp from URLFactoryTests class

--- src/test/java/edu/bsu/cs222/nba/player/stats/model/URLFactoryTests.java
+++ src/test/java/edu/bsu/cs222/nba/player/stats/model/URLFactoryTests.java
@@ -1,24 +1,11 @@
 package edu.bsu.cs222.nba.player.stats.model;
 
-import com.jayway.jsonpath.Configuration;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.InputStream;
 
 public class URLFactoryTests {
-
-    private Object playerListDoc;
-    private Object lbjStatsDoc;
-
-    @BeforeEach
-    public void setup() {
-        InputStream playerListInputStream  = Thread.currentThread().getContextClassLoader().getResourceAsStream("PlayerList2018.json");
-        InputStream lbjInputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("LeBronJamesStats2018.json");
-        playerListDoc = Configuration.defaultConfiguration().jsonProvider().parse(playerListInputStream, "UTF-8");
-        lbjStatsDoc = Configuration.defaultConfiguration().jsonProvider().parse(lbjInputStream, "UTF-8");
-    }
 
     @Test
     public void testCreatePlayerListUrl() throws Exception {
