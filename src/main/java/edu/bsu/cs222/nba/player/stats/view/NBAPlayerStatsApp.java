3e73f69e4c1332bb9718d1530b86f060d1ae0a92
Bryce Woody <bawoody@bsu.edu>
2020-04-01 14:48:56 -0400

    Implement GridPane to the GUI

--- src/main/java/edu/bsu/cs222/nba/player/stats/view/NBAPlayerStatsApp.java
+++ src/main/java/edu/bsu/cs222/nba/player/stats/view/NBAPlayerStatsApp.java
@@ -4,10 +4,12 @@ import edu.bsu.cs222.nba.player.stats.model.*;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -19,10 +21,20 @@ public class NBAPlayerStatsApp extends Application{
     private Label teamLabel;
     private Label nameLabel;
     private Label yearLabel;
+    private Label ppgLabel;
+    private Label apgLabel;
+    private Label rpgLabel;
+    private Label topgLabel;
+    private Label spgLabel;
+    private Label bpgLabel;
+    private Label fgpLabel;
+    private Label ftpLabel;
+    private Label tpmLabel;
     private ComboBox<String> teams = new ComboBox<>();
     private ComboBox<String> player = new ComboBox<>();
     private ComboBox<Integer> year = new ComboBox<>();
-    private final TextArea statsArea = new TextArea();
+    private GridPane pane = new GridPane();
+    private GridPane statsPane = new GridPane();
     private final ListOfPlayers playerList = ListOfPlayers.createEmptyListOfPlayers();
     private Map<String, String> fullPlayerList = new HashMap<>();
     private final URLCreator url = URLCreator.createEmptyUrl();
@@ -32,6 +44,7 @@ public class NBAPlayerStatsApp extends Application{
     private Button rosterButton;
     private Button seasonButton;
     private Button statsButton;
+    private PlayerStats playerStats;
 
 
     public NBAPlayerStatsApp() {
@@ -42,7 +55,7 @@ public class NBAPlayerStatsApp extends Application{
     @Override
     public void start(Stage stage) throws Exception {
         stage.setTitle("NBA Player Stats");
-        createLabels();
+        createInputLabels();
         setAllPromptText();
         createButtons();
         setUneditable();
@@ -65,10 +78,24 @@ public class NBAPlayerStatsApp extends Application{
             }
         });
         statsButton.setOnAction(event -> {
+            statsPane.getChildren().clear();
             try {
                 setStats();
+                createStatsLabels();
+                setStatsPaneConstraints();
+                statsPane.getChildren().addAll(ppgLabel, apgLabel, rpgLabel, topgLabel,
+                        spgLabel, bpgLabel, fgpLabel, ftpLabel, tpmLabel);
                 getHeadshot();
                 getLogo();
+
+                // Dr. G. slush space here!
+
+                // First, clear the results area
+                //statsPane.getChildren().clear();
+
+               // statsPane.getChildren().add(new PlayerStatsView(playerStats));
+                // end
+
             } catch (Exception e) {
                 Alert alert = new Alert(Alert.AlertType.WARNING);
                 alert.setTitle("Alert");
@@ -78,8 +105,20 @@ public class NBAPlayerStatsApp extends Application{
             }
         });
 
-        HBox resultBox = new HBox(headshotView, logoView, statsArea);
-        VBox parent = new VBox(createInputBox(), resultBox);
+        pane.setPadding(new Insets(20,20,20,20));
+        pane.setVgap(10);
+        pane.setHgap(10);
+
+        GridPane.setConstraints(headshotView,0,0);
+        GridPane.setConstraints(logoView,1,0);
+        GridPane.setConstraints(statsPane,2,0);
+        pane.getChildren().addAll(logoView, headshotView, statsPane);
+
+        statsPane.setPadding(new Insets(20,20,20,20));
+        statsPane.setVgap(10);
+        statsPane.setHgap(10);
+
+        VBox parent = new VBox(createInputBox(), pane);
         Scene scene = new Scene(parent);
         stage.setScene(scene);
         stage.show();
@@ -122,8 +161,7 @@ public class NBAPlayerStatsApp extends Application{
 
     private void setStats() throws Exception {
         playerStream = url.createPlayerProfileStream(Integer.parseInt(fullPlayerList.get(player.getValue())));
-        PlayerStats playerStats = PlayerParser.withStream(playerStream).andYear(year.getValue()).parse();
-        statsArea.setText(playerStats.toString());
+        playerStats = PlayerParser.withStream(playerStream).andYear(year.getValue()).parse();
     }
 
     private void getHeadshot() throws Exception {
@@ -135,7 +173,6 @@ public class NBAPlayerStatsApp extends Application{
     private void setUneditable() {
         player.setEditable(false);
         year.setEditable(false);
-        statsArea.setEditable(false);
         seasonButton.setDisable(true);
         statsButton.setDisable(true);
     }
@@ -152,9 +189,33 @@ public class NBAPlayerStatsApp extends Application{
         year.setPromptText("Select a season");
     }
 
-    private void createLabels() {
+    private void createInputLabels() {
         teamLabel = new Label("Team ");
         nameLabel = new Label("Player");
         yearLabel = new Label("Active Seasons ");
+    }
+
+    private void createStatsLabels(){
+        ppgLabel = new Label("PPG:  " + playerStats.getPointsPerGame());
+        apgLabel = new Label("APG:  " + playerStats.getAssistsPerGame());
+        rpgLabel = new Label("RPG:  " + playerStats.getReboundsPerGame());
+        topgLabel = new Label("TOPG:  " + playerStats.getTurnOversPerGame());
+        spgLabel = new Label("SPG:  " + playerStats.getStealsPerGame());
+        bpgLabel = new Label("BPG:  " + playerStats.getBlocksPerGame());
+        fgpLabel = new Label("FGP:  " + playerStats.getFieldGoalPercentage());
+        ftpLabel = new Label("FTP:  " + playerStats.getFreeThrowPercentage());
+        tpmLabel = new Label("TPM:  " + playerStats.getThreePointers());
+    }
+
+    private void setStatsPaneConstraints(){
+        GridPane.setConstraints(ppgLabel,0,0);
+        GridPane.setConstraints(apgLabel,0,1);
+        GridPane.setConstraints(rpgLabel,0,2);
+        GridPane.setConstraints(topgLabel,0,3);
+        GridPane.setConstraints(spgLabel,0,4);
+        GridPane.setConstraints(bpgLabel,0,5);
+        GridPane.setConstraints(fgpLabel,0,6);
+        GridPane.setConstraints(ftpLabel,0,7);
+        GridPane.setConstraints(tpmLabel,0,8);
     }
 }
