7f41514964ac7aac950322a3bf1764bef853ce86
Bryce Woody <bawoody@bsu.edu>
2020-04-23 15:07:21 -0400

    Refactor NBAPlayerStatsApp

--- src/main/java/edu/bsu/cs222/nba/player/stats/view/NBAPlayerStatsApp.java
+++ src/main/java/edu/bsu/cs222/nba/player/stats/view/NBAPlayerStatsApp.java
@@ -2,13 +2,17 @@ package edu.bsu.cs222.nba.player.stats.view;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 public class NBAPlayerStatsApp extends Application {
+
+    Button differenceButton;
 
 
     public static void main(String[] args) { launch(args); }
@@ -20,11 +24,20 @@ public class NBAPlayerStatsApp extends Application {
         boolean PLAYER_TWO = true;
         UIController leftContainer = new UIController(PLAYER_ONE);
         UIController rightContainer = new UIController(PLAYER_TWO);
-        HBox containers = new HBox(leftContainer, rightContainer);
-        Button differenceButton = new DifferenceButton(leftContainer, rightContainer).makeSeeDifferenceButton();
-        VBox ui = new VBox(containers, differenceButton);
+        leftContainer.setPadding(new Insets(0,0,0,28));
+        HBox containers = new HBox(20,leftContainer, rightContainer);
+        differenceButton = new DifferenceButton(leftContainer, rightContainer).makeSeeDifferenceButton();
+        VBox ui = new VBox(10,containers, differenceButton);
+        ui.setPadding(new Insets(20,0,0,0));
+        formatButton();
+        ui.setAlignment(Pos.TOP_CENTER);
         ui.setBackground(new Background(new BackgroundFill(Color.GAINSBORO, CornerRadii.EMPTY, Insets.EMPTY)));
-        stage.setScene(new Scene(ui, 1000, 700));
+        stage.setScene(new Scene(ui, 925, 700));
         stage.show();
+    }
+
+    private void formatButton(){
+        differenceButton.setPrefSize(220,30);
+        differenceButton.setTextAlignment(TextAlignment.CENTER);
     }
 }
